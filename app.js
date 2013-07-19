1750960d0a1d89d67ab9c277b3ab852cba061a80
Gal Hochberg <hochbergg@gmail.com>
2013-07-20 00:09:40 +0300

    Entry page view, HTML generated by jade.

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
   , data = require('./routes/data')
+  , entry = require('./routes/entry')
   , http = require('http')
   , path = require('path');
 
@@ -31,6 +32,7 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.get('/data', data.list);
 app.get('/users', user.list);
+app.get('/entry', entry.show);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
