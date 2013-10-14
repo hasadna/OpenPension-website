f3ead234d6045daf2980f288208ba752a7e2d086
Shakhal Levinson <shakhalevinson@gmail.com>
2013-10-14 04:30:07 +0200

    clean up code and fix typos

--- routes/entry.js
+++ routes/entry.js
@@ -8,14 +8,8 @@ var metaTable = require('../common/MetaTable').getMetaTable();
 
 exports.show = function(req, res){
 
-  //var filterParams = new Array();
   
   var filter = Filter.fromGetRequest(req);
-  
-  //for(var key in filter) {
-  //  var value = filterParams[key];
-  //  qs += encodeURIComponent(key) + "=" + encodeURIComponent(value) + "&";
-  // }
 
   
   DAL.groupBySummaries(filter,
@@ -23,10 +17,7 @@ exports.show = function(req, res){
 	
 		groups = DataNormalizer.normalizeData(groups);
 		total = DataNormalizer.convertNumberToWords(groups['total_sum']);
-		//res.write(JSON.stringify(e));
-	    // TBD : change to not 1. 	
-		var render = true; //for debugging
-		if (render)
+
 		res.render('entry',{
 	        entry: { title: "מופקדים בקופות הגמל", total_value: JSON.stringify(filter) },
 	        filter: filter,
@@ -34,7 +25,6 @@ exports.show = function(req, res){
 	        groups: groups,
 	        req: req	
       	});
-		else res.end();
     }
   );
   
--- routes/index.js
+++ routes/index.js
@@ -7,28 +7,19 @@ var metaTable = require('../common/MetaTable').getMetaTable();
 
 exports.show = function(req, res){
 
-  //var filterParams = new Array();
   
   var filter = Filter.fromGetRequest(req);
-  
-
-  filter.addConstraint("report_year","2013");
-  filter.addConstraint("managing_body","migdal");
 
   DAL.groupBySummaries(filter,
     function(groups){	
 		groups = DataNormalizer.normalizeData(groups);
 	    total = DataNormalizer.convertNumberToWords(groups['total_sum']);
-		//res.write(JSON.stringify(e));
-	    // TBD : change to not 1. 	
-		var render = true; //for debugging
-		if (render)
-			res.render('index',{
-		        entry: { total_value: total },
-		        groups: groups,     
-		        req: req	
-	      	});
-		else res.end();
+
+		res.render('index',{
+	        entry: { total_value: total },
+	        groups: groups,     
+	        req: req	
+      	});
     }
   );
   
