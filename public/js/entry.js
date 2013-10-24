76cefa98750e0e34dae0f794c5560670a5d07fbc
Shakhal Levinson <shakhalevinson@gmail.com>
2013-10-24 02:19:40 +0200

    add bootstrap select picker plugin http://silviomoreto.github.io/bootstrap-select/

--- public/js/entry.js
+++ public/js/entry.js
@@ -1,27 +1,6 @@
 $(function(){
 
-    var VISIBLE_ITEMS = 3;
-    /* add button, hide extra items*/      
-    $('.bargraph').each(function() {
-        var $list = $(this);
-        var listSize = $list.children('.bargraph-entry').length;
-       	if (listSize > VISIBLE_ITEMS ){
-	       	$list.append('<div class="bargraph-entry more_less"> <a class="bargraph-entry-name">הצג הכל</a><div class="bargraph-entry-value">...<div style="width: 0%;" class="bargraph-entry-bar"></div></div></div>')
-	       	$list.find('.bargraph-entry:gt('+VISIBLE_ITEMS+')').hide();
-	       	$('.more_less').show();
-	    }
-    });
-
-    /* button click handler*/
-    $('.more_less').click(function() {
-        var $btn = $(this);
-        $btn.parent().find('.bargraph-entry:gt('+VISIBLE_ITEMS+')').not('.more_less').slideToggle(400, 
-        	function(){
-				$btnname = $btn.children('.bargraph-entry-name');
-				$btnname.text($btnname.text() == 'הצג הכל' ? 'פחות' : 'הצג הכל');   
-	        });    
-	   	
-    });
+    $('#select_group_by').selectpicker();
 
     $('#select_group_by').on('change', function() {
       setConstraint("group_by",this.value);
@@ -41,12 +20,7 @@ function addConstraint(key,value){
   //get next grouping category
   var group_by = Categories.getGroupingCategory("default",filter);
 
-  if(typeof group_by == "undefined"){
-    filter.removeField("group_by");
-  }
-  else{
   filter.setConstraint("group_by",group_by);
-  }
 
   //convert filter back to query string, and apply location
   window.location.href = filter.toQueryString();
