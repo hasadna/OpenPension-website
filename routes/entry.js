5be109ccc7ee07226c1445fa9ecd2fbd7cbd1134
gilad kahala <gilad.kahala@gmail.com>
2013-11-11 21:33:06 +0200

    title

--- routes/entry.js
+++ routes/entry.js
@@ -28,12 +28,49 @@ exports.show = function(req, res){
 
 
 
+					
   //show data only for last quarter
   //TODO: get last quarter from DB
   filter.addConstraint("report_year","2013");
   filter.addConstraint("report_qurater","2");
-  
 
+						
+  var title = "";
+  var onlyManagingBody = 0;
+  var nothingIsChosen = 0;
+  var addTheWordNechasim =0;
+  
+  // TBD add categories 
+  var managing_body = filter.getConstraintData('managing_body');
+  var instrument_type = filter.getConstraintData('instrument_type');
+  var industry = filter.getConstraintData('industry');
+  var currency = filter.getConstraintData('currency');
+  var rating = filter.getConstraintData('rating');
+  var instrument_sub_type = filter.getConstraintData('instrument_sub_type');
+  var instrument_id = filter.getConstraintData('instrument_id');
+
+  // nothing is chosen by the user
+  if (managing_body== "" && instrument_type=="" && industry=="" && currency=="" && rating=="" && instrument_sub_type=="" && instrument_id==""  ) {
+	nothingIsChosen = 1; 
+  }  
+  // only managing body is active 
+  if (managing_body!= "" && instrument_type=="" && industry=="" && currency=="" && rating=="" && instrument_sub_type=="" && instrument_id==""  ) {
+	onlyManagingBody = 1; 
+  }
+  // If managing body or instrument is not chosen add the word 'instruments' (nechasim) to have a NOSSE 
+  if (managing_body== "" && instrument_type=="" ) {
+	addTheWordNechasim = 1; 
+  }
+  
+  title +=  (managing_body != "")?onlyManagingBody?"כמה כסף מנהלת " + managing_body : "כמה כסף משקיעה " + managing_body:"כמה כסף מושקע";
+  title +=  (instrument_type != "")?" ב" + instrument_type :"";
+  title +=  (addTheWordNechasim)?" בנכסים" : "";
+  title +=  (industry != "")?" בענף ה" + industry :"";	
+  title +=  (currency != "")?" שנקנו ב" + currency :"";
+  title +=  (rating != "")?" בדירוג " + rating :"";
+  
+  
+  
   DAL.groupBySummaries(filter,
     function(groups){
     DAL.groupByQuarters(filter,
@@ -42,6 +79,7 @@ exports.show = function(req, res){
         total = DataNormalizer.convertNumberToWords(groups['total_sum']);
 
         res.render('entry',{
+			  title : title,
               filter: filter,
               quarters: quarters,
               total: total,
