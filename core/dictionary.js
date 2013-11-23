fe91619adde5e273f6cda6535e05bb3743c1286c
Shakhal Levinson <shakhalevinson@gmail.com>
2013-11-23 20:08:59 +0200

    add values to dicitonary

--- core/dictionary.js
+++ core/dictionary.js
@@ -11,9 +11,13 @@ for(var index in englishColumns)
 	dictionary[englishColumns[index]] = hebrewColumns[index];
 }
 
+dictionary['activity_industry'] = 'ענף פעילות';
 dictionary['managing_body'] = 'גוף מוסדי';
 dictionary['instrument_type'] = 'סוג נכס';
 dictionary['instrument_sub_type'] = 'תת סוג נכס';
+dictionary['issuer'] = 'מנפיק';
+dictionary['instrument_name'] = 'שם נכס';
+dictionary['reference_index'] = 'מדד יחסי';
 
 var translate = function(word){
 	if (dictionary.hasOwnProperty(word)){
