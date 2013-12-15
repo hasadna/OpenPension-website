578e72a1f0bf8fa90b5b464854313c4cec647329
fuchsr <ronit.fuchs@gmail.com>
2013-12-15 13:12:53 +0200

    Update view to last version creating pention_data_all

--- sql/joined_view.sql
+++ sql/joined_view.sql
@@ -1,18 +1,19 @@
-CREATE OR REPLACE VIEW pension_data_all AS 
- SELECT debug.managing_body, 
-    debug.fund, 
-    funds.fund_name, 
+CREATE OR REPLACE VIEW pention_data_all AS 
+ SELECT 
+    debug.managing_body,
+    debug.fund,
+    funds.fund_name,
     debug.report_year, 
     debug.report_qurater, 
     debug.instrument_type, 
     debug.instrument_sub_type, 
-    debug.instrument_id, 
-    COALESCE(ext.instrument_name, debug.instrument_symbol) AS instrument_name, 
+    debug.instrument_id,
+    COALESCE(ext.instrument_name,debug.instrument_symbol) as instrument_name,
     ext.issuer, 
-    COALESCE(ext.activity_industry, debug.industry) AS activity_industry, 
-    debug.currency, 
-    COALESCE(debug.fair_value, 0::numeric) * 1000::numeric AS fair_value, 
-    COALESCE(debug.market_cap, 0::numeric) * 1000::numeric AS market_cap, 
+    COALESCE(ext.activity_industry,debug.industry) as activity_industry,
+    debug.currency,     
+    (COALESCE(debug.fair_value,0))*1000 as fair_value, 
+    (COALESCE(debug.market_cap,0))*1000 as market_cap, 
     debug.rate_of_fund, 
     debug.rating_agency, 
     ext.reference_index, 
@@ -23,12 +24,13 @@ CREATE OR REPLACE VIEW pension_data_all AS
     debug.rate, 
     debug.yield, 
     debug.rating, 
-    debug.par_value, 
+    debug.par_value,
     debug.tmp_name, 
     debug.underlying_asset, 
     debug.type_of_asset, 
-    debug.rate_of_ipo
+    debug.rate_of_ipo 
    FROM debug
-   LEFT JOIN ext ON debug.instrument_id::text = ext.instrument_id::text
-   LEFT JOIN funds ON debug.fund = funds.fund_id
-  WHERE debug.instrument_id::text <> ''::text AND debug.instrument_id IS NOT NULL AND debug.instrument_symbol::text <> ''::text AND debug.instrument_symbol IS NOT NULL;
+   LEFT JOIN ext ON (debug.instrument_id::text = ext.instrument_id::text)
+   LEFT JOIN funds ON (debug.fund = funds.fund_id)
+  WHERE (debug.instrument_id::text <> ''::text AND debug.instrument_id IS NOT NULL)
+	and (debug.instrument_symbol::text <> ''::text AND debug.instrument_symbol IS NOT NULL);
