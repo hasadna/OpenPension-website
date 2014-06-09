1e2be1421d7e4bd7dd652fd256afb221b3591b33
fuchsr <ronit.fuchs@gmail.com>
2014-06-09 20:37:51 +0300

    Add liquidity data to the view
    join the table liquidity to translate the instrument_symbol and instrument_type to liquidity and asset_type

--- sql/joined_view.sql
+++ sql/joined_view.sql
@@ -6,7 +6,7 @@ CREATE OR REPLACE VIEW pension_data_all AS
     production.report_year, 
     production.report_qurater, 
     production.instrument_type, 
-    production.instrument_sub_type, 
+    production.instrument_sub_type,
     production.instrument_id,
     case 	when production.instrument_sub_type in ('חוזים עתידיים', 'אופציות')	then production.instrument_sub_type
 		else COALESCE(ext.instrument_name,production.instrument_symbol,production.instrument_id)
@@ -25,6 +25,7 @@ CREATE OR REPLACE VIEW pension_data_all AS
 		when production.instrument_type in ('מזומנים','פקדונות')	then 'ללא סיווג'::character varying(128)
 		else 							     'ללא סיווג'::character varying(128)
     end					as activity_industry,
+--    production.currency,     
     COALESCE(production.currency,'NIS'::character varying(128)) 	as currency,     
     (COALESCE(production.fair_value,0))*1000 	as fair_value, 
     (COALESCE(production.market_cap,0))*1000 	as market_cap, 
@@ -37,7 +38,7 @@ CREATE OR REPLACE VIEW pension_data_all AS
     production.date_of_revaluation, 
     production.rate,
     production.yield, 
-    case	when production.rating_agency = 'פנימי' 			then 'פנימי'::character varying(128)
+    case	when production.rating_agency = 'פנימי' 		then 'פנימי'::character varying(128)
 		when production.rating = 'Aaa' 				then 'AAA'::character varying(128)
 		when production.rating = 'Aa1' 				then 'AA+'::character varying(128)
 		when production.rating = 'Aa2' 				then 'AA'::character varying(128)
@@ -45,9 +46,9 @@ CREATE OR REPLACE VIEW pension_data_all AS
 		when production.rating = 'A1' 				then 'A+'::character varying(128)
 		when production.rating = 'A2' 				then 'A'::character varying(128)
 		when production.rating = 'A3' 				then 'A-'::character varying(128)
-		when production.rating = 'Baa1' 				then 'BBB+'::character varying(128)
-		when production.rating = 'Baa2' 				then 'BBB'::character varying(128)
-		when production.rating = 'Baa3' 				then 'BBB-'::character varying(128)
+		when production.rating = 'Baa1' 			then 'BBB+'::character varying(128)
+		when production.rating = 'Baa2' 			then 'BBB'::character varying(128)
+		when production.rating = 'Baa3' 			then 'BBB-'::character varying(128)
 		when production.rating = 'Ba1' 				then 'BB+'::character varying(128)
 		when production.rating = 'Ba2' 				then 'BB'::character varying(128)
 		when production.rating = 'Ba3' 				then 'BB-'::character varying(128)
@@ -55,9 +56,9 @@ CREATE OR REPLACE VIEW pension_data_all AS
 		when production.rating = 'B2' 				then 'B'::character varying(128)
 		when production.rating = 'B3' 				then 'B-'::character varying(128)
 		when production.rating = 'Caa' 				then 'CCC+'::character varying(128)
-		when production.rating = 'Caa2' 				then 'CCC'::character varying(128)
+		when production.rating = 'Caa2' 			then 'CCC'::character varying(128)
 		when production.rating = 'Ca' 				then 'CCC'::character varying(128)
-		when production.rating = 'C' 					then 'CCC-'::character varying(128)
+		when production.rating = 'C' 				then 'CCC-'::character varying(128)
 		when production.rating = 'RF' 				then 'AAA'::character varying(128)
 		when production.rating = 'NR' 				then 'לא מדורג'::character varying(128)
 		else 							     production.rating
@@ -66,13 +67,18 @@ CREATE OR REPLACE VIEW pension_data_all AS
     production.tmp_name, 
     production.underlying_asset, 
     production.type_of_asset, 
-    production.rate_of_ipo 
+    production.rate_of_ipo, 
+    liquidity.liquidity,
+    liquidity.asset_type 
    FROM production
    LEFT JOIN ext ON (production.instrument_id::text = ext.instrument_id::text)
    LEFT JOIN funds ON (production.fund = funds.fund_id)
+   LEFT JOIN liquidity ON (liquidity.instrument_type::text = production.instrument_type::text 
+			and (liquidity.instrument_sub_type::text = production.instrument_sub_type::text
+				or production.instrument_sub_type is null))
   WHERE -- (production.instrument_id::text <> ''::text AND production.instrument_id IS NOT NULL)
 	 (production.instrument_symbol::text <> ''::text AND production.instrument_symbol IS NOT NULL)
-	 and instrument_type<>'סכום נכסי הקרן';
+	 and production.instrument_type<>'סכום נכסי הקרן';
 
 ALTER TABLE pension_data_all
  OWNER TO op;
