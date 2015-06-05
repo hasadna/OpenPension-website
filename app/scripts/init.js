6712e4ece71ca26a32f0208bca1e5e4f7e967c65
Shakhal Levinson <shakhalevinson@gmail.com>
2015-06-05 18:31:46 +0100

    Add bootstrap select

--- app/scripts/init.js
+++ app/scripts/init.js
@@ -3,7 +3,7 @@ require.config({
     baseUrl: "/scripts",
 
     /* starting point for application */
-    deps: ['backbone.marionette', 'bootstrap', 'main'],
+    deps: ['backbone.marionette', 'bootstrap', 'main', 'bootstrap-select'],
 
 
     shim: {
@@ -29,9 +29,9 @@ require.config({
         'backbone.marionette': '../bower_components/marionette/lib/core/backbone.marionette',
         'backbone.wreqr': '../bower_components/backbone.wreqr/lib/backbone.wreqr',
         'backbone.babysitter': '../bower_components/backbone.babysitter/lib/backbone.babysitter',
-
         /* alias the bootstrap js lib */
         bootstrap: 'vendor/bootstrap',
+        'bootstrap-select': '../bower_components/bootstrap-select/dist/js/bootstrap-select',
 
         /* Alias text.js for template loading and shortcut the templates dir to tmpl */
         text: '../bower_components/requirejs-text/text',
