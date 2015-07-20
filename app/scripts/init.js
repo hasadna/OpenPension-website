7439d9f4f80784eb85f96dc3de4566c5c1911980
Shlomi Fruchter <shlomi.fruchter@gmail.com>
2015-07-20 20:05:13 +0300

    fix jquery path in init.js

--- app/scripts/init.js
+++ app/scripts/init.js
@@ -25,7 +25,7 @@ require.config({
     },
 
     paths: {
-        jquery: '../bower_components/jquery/jquery',
+        jquery: '../bower_components/jquery/dist/jquery',
         backbone: '../bower_components/backbone/backbone',
         underscore: '../bower_components/underscore/underscore',
 
