70d063c1878b852b74720e1384ea72208ac5c9d7
Shakhal Levinson <shakhalevinson@gmail.com>
2015-11-28 02:37:28 +0200

    fix naming, remove unused

--- app/views/investment/InvestmentView.js
+++ app/views/investment/InvestmentView.js
@@ -5,7 +5,7 @@ define(function(require) {
   var Marionette = require('backbone.marionette');
   var template = require('hbs!/templates/portfolio');
   var PortfolioHeaderView = require('../../views/portfolio/PortfolioHeaderView');
-  var PortfolioContentView = require('../../views/investment/InvestmentContentView');
+  var InvestmentContentView = require('../../views/investment/InvestmentContentView');
   var Sparkline = require('Sparkline');
 
   return Backbone.Marionette.LayoutView.extend({
@@ -15,12 +15,11 @@ define(function(require) {
     template: template,
     regions: {
   		portfolio_header: '#page-header',
-  		content: '#portfolio-content',
-  		footer: 'footer'
+  		content: '#portfolio-content'
   	},
   	onBeforeShow: function() {
     		this.showChildView('portfolio_header', new PortfolioHeaderView(this.options));
-    		this.showChildView('content', new PortfolioContentView(this.options));
+    		this.showChildView('content', new InvestmentContentView(this.options));
   	},
     onAttach: function(){
     }
