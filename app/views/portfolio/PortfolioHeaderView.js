78a8586e650b44889f78a1cbd4e32015f5e7c20a
Shakhal Levinson <shakhalevinson@gmail.com>
2015-12-29 12:05:02 +0200

    handle breadcrumbs

--- app/views/portfolio/PortfolioHeaderView.js
+++ app/views/portfolio/PortfolioHeaderView.js
@@ -12,10 +12,42 @@ define(function(require) {
     var drillDown = filter.getDrillDown();
     return _.map(drillDown, function(field){
         return {
+           value_eng: filter.getConstraintData(field)[0],
            value: Dictionary.translate(filter.getConstraintData(field)[0]),
            field: Dictionary.translate(field)
         }
     })
+  }
+
+  function breadCrumbsClick(event) {
+
+    var value = $(event.target).data('value');
+    
+    //generate filter from query string
+    var filter = Filter.fromQueryString(window.location.search);
+    
+    var constraintFields = filter.getConstrainedFields();
+
+    for ( var index = constraintFields.length - 1; index >= 0; index-- ) {
+
+        var field = filter.getConstrainedFields()[index];
+
+        // if bread crumb is equal to field, stop removing
+        if (value == filter.getConstraintData(field)) {
+            break;
+        }
+        
+        //remove all fields other than quarter, year and group_by
+        if (field == 'report_qurater' || 
+            field == 'report_year' ){
+            continue;
+        }
+
+        filter.removeField(field);
+
+    }
+
+    window.location.hash = '#/portfolio'+filter.toQueryString();
   }
 
   return Marionette.ItemView.extend({
@@ -25,7 +57,18 @@ define(function(require) {
     },
     serializeData: function(){
       var breadcrumbs = getBreadcrumbs(this.filter);
-      var lastCrumb = breadcrumbs.pop();
+      var groupByField = this.filter.getConstraintData('group_by')[0];
+
+      var lastCrumb;
+      if (groupByField){ //add group by to bread crumbs
+        lastCrumb = {
+          value: 'כל ' + Dictionary.plurals[groupByField],
+          field: ''
+        }  
+      }
+      else{
+        lastCrumb = breadcrumbs.pop();
+      }
 
     	return {
   			report_type: TitleGenerator.getReportTypeHeb(this.filter),
@@ -39,6 +82,9 @@ define(function(require) {
         title: function (name) {
             return Filter;
         }
+    },
+    events: {
+      "click .breadcrumb": breadCrumbsClick 
     }
   });
 });
