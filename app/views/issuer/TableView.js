8a7cfa020adf4973c3109e3a81d6793b563b34bf
Shakhal Levinson <shakhalevinson@gmail.com>
2015-12-29 02:53:17 +0200

    remove unused

--- app/views/issuer/TableView.js
+++ app/views/issuer/TableView.js
@@ -1,6 +1,6 @@
 define(function(require) {
   'use strict';
-  var $ = require('jquery');
+  
   var Backbone = require('backbone');
   var Marionette = require('backbone.marionette');
   var TableItemView = require('../../views/issuer/TableItemView.js');
